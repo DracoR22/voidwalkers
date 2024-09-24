ff759d349f6608e63b544fc3a5af1b6f7763fa5f
el raton <rdraco039@gmail.com>
2024-09-23 22:50:52 -0500

    added sone walls

--- src/house/systems/floor.rs
+++ src/house/systems/floor.rs
@@ -33,14 +33,55 @@ pub fn spawn_floor_system(mut commands: Commands, asset_server: Res<AssetServer>
         }
     }
 
+    let initial_position = Vec3::new(1577.0, 0.0, -570.0);
+    let spacing_x = 187.0; // The amount to subtract from the X position for each wall
+
+    for i in 0..9 {
+        let x_position = initial_position.x - (i as f32) * spacing_x;
+        commands.spawn(SceneBundle {
+            scene: wall_model.clone(),
+            transform: Transform {
+                translation: Vec3::new(x_position, initial_position.y, initial_position.z),
+                scale: Vec3::splat(100.0),
+               
+                ..default()
+            },
+            ..default()
+        })
+        .insert(Collider::cuboid(1.0, 100.0, 0.2))
+        .insert(RigidBody::Fixed);
+    }
+
+   for y in 0..9 {
+    let z_position = -365.0 + (y as f32) * spacing_x;
     commands.spawn(SceneBundle {
         scene: wall_model.clone(),
         transform: Transform {
-            translation: Vec3::new(400.0, 70.0, 0.0),
+            translation: Vec3::new(107.0, 0.0, z_position),
             scale: Vec3::splat(100.0),
-            rotation: Quat::from_rotation_x(-std::f32::consts::FRAC_PI_2), // Rotate floor to lie flat
+            rotation: Quat::from_rotation_y(89.53),
             ..default()
         },
         ..default()
-    });
+    })
+    .insert(Collider::cuboid(1.0, 100.0, 0.2))
+    .insert(RigidBody::Fixed);
+   }
+
+  for k in 0..9 {
+    let x_position = 320.0 + (k as f32) * spacing_x;
+    commands.spawn(SceneBundle {
+        scene: wall_model.clone(),
+        transform: Transform {
+            translation: Vec3::new(x_position, 0.0, 1100.0),
+            scale: Vec3::splat(100.0),
+            rotation: Quat::from_rotation_y(std::f32::consts::PI),
+            ..default()
+        },
+        ..default()
+    })
+    .insert(Collider::cuboid(1.0, 100.0, 0.2))
+    .insert(RigidBody::Fixed);
+  }
+    
 }
