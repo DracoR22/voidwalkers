28bb6063a45f6857c4b1de0e9937a943cb21d6f8
el raton <rdraco039@gmail.com>
2024-10-05 23:27:07 -0500

    adding more ways of muzzle flash

--- src/main.rs
+++ src/main.rs
@@ -24,6 +24,9 @@ impl Material for MuzzleFlashMaterial {
     }
 }
 
+#[derive(Component)]
+pub struct HasFlash;
+
 fn main() {
     App::new()
     .add_plugins(DefaultPlugins.set(LogPlugin {
@@ -45,7 +48,7 @@ fn main() {
     .add_plugins(GameUIPugin)
     .init_state::<CurrentWeapon>()
     .add_systems(Startup, spawn_cube_system)
-    .add_systems(Startup, setup_muzzle_flash)
+    .add_systems(Update, setup_muzzle_flash)
     .add_systems(Update, update_muzzle_flash)
     .run();
 }
@@ -61,6 +64,7 @@ pub fn setup_muzzle_flash(
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<MuzzleFlashMaterial>>,
     asset_server: Res<AssetServer>,
+    player_query: Query<(Entity, Option<&HasFlash>), With<Player>>,
 ) {
    
     let material = materials.add(MuzzleFlashMaterial {
@@ -68,19 +72,46 @@ pub fn setup_muzzle_flash(
         color_texture: asset_server.load("textures/muzzle-flash.png"),
     });
 
- // Muzzle flash (using a flat quad)
- commands.spawn((
-    MaterialMeshBundle {
-        mesh: meshes.add(Mesh::from(shape::Quad::new(Vec2::new(20.2, 20.2)))),
-        material,
-        transform: Transform::from_xyz(700.0, 100.0, 0.0),
-        visibility: Visibility::Hidden,
-        ..default()
-    },
-    MuzzleFlash {
-        timer: Timer::from_seconds(0.05, TimerMode::Once),
-    },
-));
+   if let Ok((player_entity, has_flash)) = player_query.get_single() {
+    //  commands.entity(player_entity).with_children(|parent| {
+    //       parent.spawn((
+    //         MaterialMeshBundle {
+    //             mesh: meshes.add(Mesh::from(shape::Quad::new(Vec2::new(200.2, 200.2)))),
+    //             material,
+    //             transform: Transform::from_xyz(0.2, 85.0, -67.0),
+    //             visibility: Visibility::Visible,
+    //             ..default()
+    //         },
+    //         MuzzleFlash {
+    //             timer: Timer::from_seconds(1000.05, TimerMode::Once),
+    //         },
+    //     ));
+    //  });
+    
+    commands.entity(player_entity).with_children(|parent| {
+       if has_flash.is_none() {
+        parent.spawn((
+            SceneBundle {
+                scene: asset_server.load("models/muzzle-flash.glb"),
+                transform: Transform {
+                    scale: Vec3::splat(300.0), 
+                    translation: Vec3::new(10.2, 85.0, 7.0), 
+                    rotation: Quat::from_rotation_y(std::f32::consts::PI), 
+                    ..default()
+                },
+                visibility: Visibility::Visible,
+                ..default()
+            },
+            MuzzleFlash {
+                           timer: Timer::from_seconds(1000.05, TimerMode::Once),
+                   },
+        ));
+        }
+     });
+
+     commands.entity(player_entity).insert(HasFlash);
+   };
+ 
 }
 
 pub fn update_muzzle_flash(
@@ -89,29 +120,41 @@ pub fn update_muzzle_flash(
     mouse_input: Res<ButtonInput<MouseButton>>,
     mut param_set: ParamSet<(
         Query<(Entity, &mut MuzzleFlash, &mut Visibility, &mut Transform)>,
-        Query<&Transform, (With<Camera>, Without<MuzzleFlash>)>
+        Query<&Transform, (With<PlayerFirstPersonCamera>, Without<MuzzleFlash>)>,
     )>,
 ) {
+    // Check if the left mouse button is pressed
     if mouse_input.just_pressed(MouseButton::Left) {
-        // let camera_transform = camera_query.single();
+        // Get the camera transform
+        let camera_transform = if let Ok(transform) = param_set.p1().get_single() {
+            *transform
+        } else {
+            return; // Exit the function if there's no camera
+        };
 
-        // println!("TRANSFORM: {:?}", camera_transform.translation);
+        // Iterate over all muzzle flash entities
         for (entity, mut muzzle_flash, mut visibility, mut transform) in param_set.p0().iter_mut() {
+            // Reset the muzzle flash timer
             muzzle_flash.timer.reset();
             *visibility = Visibility::Visible;
 
-            // update translation
-            transform.translation = Vec3::new(700.0, 10.0, 0.0)
+            // Make the muzzle flash face towards the camera
+            let direction_to_camera = camera_transform.translation - transform.translation;
+            // transform.look_at(camera_transform.translation, Vec3::Y); 
+
+            // Optionally adjust position, but don't reset it to a fixed point.
+            // Only adjust this part if you want the muzzle flash to appear at the muzzle of the gun.
+            transform.translation = Vec3::new(camera_transform.translation.x * 2.0, transform.translation.y, transform.translation.z * 2.0); // REMOVE OR MODIFY AS NEEDED
         }
     }
 
-    for (entity, mut muzzle_flash, mut visibility, mut transform) in param_set.p0().iter_mut() {
+    // Update visibility based on the muzzle flash timer
+    for (entity, mut muzzle_flash, mut visibility, _) in param_set.p0().iter_mut() {
         if muzzle_flash.timer.tick(time.delta()).just_finished() {
             *visibility = Visibility::Hidden;
         }
     }
 }
-
 
 #[derive(Asset, AsBindGroup, TypePath, Clone)]
 struct LiquidMaterial {
