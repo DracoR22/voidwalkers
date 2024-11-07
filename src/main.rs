66a3f9ba56503c8bb21380a3e9534b85512831e5
el raton <rdraco039@gmail.com>
2024-11-06 23:36:02 -0500

    adding dynamic scenes

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
-use bevy::{pbr::NotShadowCaster, prelude::*};
+use std::{fs::File, io::Write};
+
+
+use bevy::{pbr::NotShadowCaster, prelude::*, tasks::IoTaskPool, utils::Duration};
 use bevy::log::LogPlugin;
 use bevy_atmosphere::plugin::AtmosphereSkyBox;
 use bevy_kira_audio::prelude::*;
@@ -28,13 +31,21 @@ impl Material for MuzzleFlashMaterial {
 
 fn main() {
     App::new()
-    .add_plugins(DefaultPlugins
+    .add_plugins((DefaultPlugins
         .set(LogPlugin {
             filter: "error".into(),
             level: bevy::log::Level::ERROR,
             ..default()
-        })
+        }), DefaultPlugins.set(AssetPlugin {
+            watch_for_changes_override: Some(true),
+            ..default()
+        }))
     )
+
+    .register_type::<ComponentA>()
+    .register_type::<ComponentB>()
+    .register_type::<ResourceA>()
+
     .add_plugins(FrameTimeDiagnosticsPlugin::default())
     .add_plugins(HanabiPlugin)
     .add_plugins(AudioPlugin)
@@ -60,44 +71,118 @@ fn main() {
     .run();
 }
 
-fn update_sky_position(
-    atmosphere_query: Query<&AtmosphereSkyBox>
-) {
-    let atmosphere = atmosphere_query.get_single().unwrap();
-    
+
+#[derive(Component, Reflect, Default)]
+#[reflect(Component)] // this tells the reflect derive to also reflect component behaviors
+struct ComponentA {
+    pub x: f32,
+    pub y: f32,
+}
+
+#[derive(Component, Reflect)]
+#[reflect(Component)]
+struct ComponentB {
+    pub value: String,
+    #[reflect(skip_serializing)]
+    pub _time_since_startup: Duration,
+}
+
+#[derive(Resource, Reflect, Default)]
+#[reflect(Resource)]
+struct ResourceA {
+    pub score: u32,
+}
+
+impl FromWorld for ComponentB {
+    fn from_world(world: &mut World) -> Self {
+        let time = world.resource::<Time>();
+        ComponentB {
+            _time_since_startup: time.elapsed(),
+            value: "Default Value".to_string(),
+        }
+    }
 }
 
 
-fn setup(
-    mut commands: Commands,
-    mut meshes: ResMut<Assets<Mesh>>,
-    mut materials: ResMut<Assets<StandardMaterial>>,
-    asset_server: Res<AssetServer>,
+// The initial scene file will be loaded below and not change when the scene is saved
+const SCENE_FILE_PATH: &str = "scenes/load_scene_example.scn.ron";
+
+// The new, updated scene data will be saved here so that you can see the changes
+const NEW_SCENE_FILE_PATH: &str = "scenes/load_scene_example-new.scn.ron";
+
+fn load_scene_system(mut commands: Commands, asset_server: Res<AssetServer>) {
+    // Spawning a DynamicSceneRoot creates a new entity and spawns new instances
+    // of the given scene's entities as children of that entity.
+    // Scenes can be loaded just like any other asset.
+    commands.spawn(DynamicSceneBundle {
+        scene: asset_server.load(SCENE_FILE_PATH),
+        ..default()
+    });
+}
+
+
+pub fn log_system(
+    query: Query<(Entity, &ComponentA), Changed<ComponentA>>,
+    res: Option<Res<ResourceA>>,
 ) {
-    let texture_handle: Handle<Image> = asset_server.load("textures/NightSky_Top.png");
-    commands.spawn((
-        PbrBundle {
-            mesh: meshes.add(Cuboid::new(100000.0, 100000.0, 100000.0)),
-            material: materials.add(StandardMaterial {
-                base_color_texture: Some(texture_handle),
-                unlit: true,
-                cull_mode: None,
-                
-                ..default()
-            }),
-            transform: Transform::from_scale(Vec3::splat(20.0)),
-            ..default()
-        },
-        NotShadowCaster,
+    for (entity, component_a) in &query {
+        info!("  Entity({})", entity.index());
+        info!(
+            "    ComponentA: {{ x: {} y: {} }}\n",
+            component_a.x, component_a.y
+        );
+    }
+    if let Some(res) = res {
+        if res.is_added() {
+            info!("  New ResourceA: {{ score: {} }}\n", res.score);
+        }
+    }
+}
+
+fn save_scene_system(world: &mut World) {
+    // Scenes can be created from any ECS World.
+    // You can either create a new one for the scene or use the current World.
+    // For demonstration purposes, we'll create a new one.
+    let mut scene_world = World::new();
+
+    // The `TypeRegistry` resource contains information about all registered types (including components).
+    // This is used to construct scenes, so we'll want to ensure that our previous type registrations
+    // exist in this new scene world as well.
+    // To do this, we can simply clone the `AppTypeRegistry` resource.
+    let type_registry = world.resource::<AppTypeRegistry>().clone();
+    scene_world.insert_resource(type_registry);
+
+    let mut component_b = ComponentB::from_world(world);
+    component_b.value = "hello".to_string();
+    scene_world.spawn((
+        component_b,
+        ComponentA { x: 1.0, y: 2.0 },
+        Transform::IDENTITY,
     ));
-}
+    scene_world.spawn(ComponentA { x: 3.0, y: 4.0 });
+    scene_world.insert_resource(ResourceA { score: 1 });
 
+    // With our sample world ready to go, we can now create our scene using DynamicScene or DynamicSceneBuilder.
+    // For simplicity, we will create our scene using DynamicScene:
+    let scene = DynamicScene::from_world(&scene_world);
 
+    // Scenes can be serialized like this:
+    let type_registry = world.resource::<AppTypeRegistry>();
+    let serialized_scene = scene.serialize_ron(type_registry).unwrap();
 
+    // Showing the scene in the console
+    info!("{}", serialized_scene);
 
-
-#[derive(Asset, AsBindGroup, TypePath, Clone)]
-struct LiquidMaterial {
-    #[uniform(0)]
-    time: f32,
+    // Writing the scene to a new file. Using a task to avoid calling the filesystem APIs in a system
+    // as they are blocking
+    // This can't work in WASM as there is no filesystem access
+    #[cfg(not(target_arch = "wasm32"))]
+    IoTaskPool::get()
+        .spawn(async move {
+            // Write the scene RON data to file
+            File::create(format!("assets/{NEW_SCENE_FILE_PATH}"))
+                .and_then(|mut file| file.write(serialized_scene.as_bytes()))
+                .expect("Error while writing scene to file");
+        })
+        .detach();
 }
